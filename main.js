f62ff03128eaf34f800398a20b72a92318658f5b
unknown <sekharctc.2011@gmail.com>
2017-08-21 00:10:13 +0530

    make

--- main.js
+++ main.js
@@ -2,29 +2,40 @@ const electron = require('electron')
 const {app, BrowserWindow, globalShortcut} = electron
 
 app.on('ready', ()=>  {
-    let win = new BrowserWindow({width:800, height:600})
-    win.loadURL(`file://${__dirname}/index.html`)
-
-    const ret = globalShortcut.register('Tab', () => {
-        console.log('CommandOrControl+X is pressed')
-    })
-
-    const ref = globalShortcut.register('1', () => {
-        console.log('CommandOrControl+A is pressed')
-    })
-
-    if (!ret) {
-        console.log('registration failed')
+    const Store = require('electron-store');
+    const store = new Store();
+    store.delete('optrackuser');
+    store.delete('optoken');
+    store.delete('last_upload');
+    if ( typeof store.get('optrackuser') == "undefined") {
+        let win = new BrowserWindow({width:800, height:600})
+        win.loadURL(`file://${__dirname}/login.html`)
     }
+    else{
+        let win = new BrowserWindow({width:800, height:600})
+        win.loadURL(`file://${__dirname}/index.html`)
+    }
+    
+    // const ret = globalShortcut.register('Tab', () => {
+    //     console.log('CommandOrControl+X is pressed')
+    // })
+
+    // const ref = globalShortcut.register('1', () => {
+    //     console.log('CommandOrControl+A is pressed')
+    // })
+
+    // if (!ret) {
+    //     console.log('registration failed')
+    // }
 
   // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+  //console.log(globalShortcut.isRegistered('CommandOrControl+X'))
 })
 
 app.on('will-quit', () => {
   // Unregister a shortcut.
-  globalShortcut.unregister('CommandOrControl+X')
+  //globalShortcut.unregister('CommandOrControl+X')
 
   // Unregister all shortcuts.
-  globalShortcut.unregisterAll()
+  //globalShortcut.unregisterAll()
 })
